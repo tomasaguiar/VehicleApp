68b4a8267689b8e22ba4101a90de0d1105259bed
Tomás Aguiar <aguiarhcode@gmail.com>
2022-05-12 22:27:20 +0100

    Auth Routes Updated

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import Main from '../pages/Main';
 import SignIn from '../pages/SignIn';
+import LogIn from '../pages/LogIn';
 
 const AuthStack = createStackNavigator();
 
@@ -9,8 +11,11 @@ const AuthRoutes = () => (
   <AuthStack.Navigator
     screenOptions={{
       headerShown: false,
+      initialRouteName: 'Main',
     }}>
-    <AuthStack.Screen name="SignIn" component={SignIn} />
+    <AuthStack.Screen name="main" component={Main} />
+    <AuthStack.Screen name="signin" component={SignIn} />
+    <AuthStack.Screen name="login" component={LogIn} />
   </AuthStack.Navigator>
 );
 
