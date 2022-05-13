7ea77941bdc7f9789d5602dd82e5d5d9aeb32650
Tomás Aguiar <aguiarhcode@gmail.com>
2022-05-13 19:02:59 +0100

    settings improved

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 
-import {CaretLeft} from 'phosphor-react-native';
+import {AuthContext} from '../../routes/provider/AuthProvider';
+
+import {
+  CaretLeft,
+  CaretRight,
+  UserCircle,
+  UserSwitch,
+} from 'phosphor-react-native';
 
 const Settings = ({navigation}) => {
+  const {user, logout} = React.useContext(AuthContext);
+
   return (
     <View style={{flex: 1, backgroundColor: '#171717'}}>
       <View style={{marginHorizontal: 38}}>
@@ -19,13 +28,58 @@ const Settings = ({navigation}) => {
             }}>
             <CaretLeft color="#999999" />
           </TouchableOpacity>
-          <Text style={{fontSize: 20, marginLeft: 10}}>Settings</Text>
+          <Text style={{fontSize: 20, marginLeft: 10}}>Definições</Text>
+        </View>
+        <View style={{marginTop: 30}}>
+          <TouchableOpacity
+            onPress={() => {
+              logout();
+            }}>
+            <View
+              style={{
+                width: '100%',
+                height: 50,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}>
+              <View>
+                <UserCircle size={35} color={'#252525'} weight="fill" />
+              </View>
+              <View style={{marginRight: 185}}>
+                <Text style={{fontSize: 16}}>Logout</Text>
+              </View>
+              <View>
+                <CaretRight color={'#404040'} />
+              </View>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{marginTop: 20}}>
+            <View
+              style={{
+                width: '100%',
+                height: 50,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}>
+              <View>
+                <UserSwitch size={35} color={'#252525'} weight="fill" />
+              </View>
+              <View style={{marginRight: 135}}>
+                <Text style={{fontSize: 16}}>Edit account</Text>
+              </View>
+              <View>
+                <CaretRight color={'#404040'} />
+              </View>
+            </View>
+          </TouchableOpacity>
         </View>
         <View
           style={{
             justifyContent: 'center',
             alignItems: 'center',
-            marginTop: 20,
+            marginTop: 500,
           }}>
           <Text>UrVehicle v.0.0.1 Pre Alpha</Text>
         </View>
