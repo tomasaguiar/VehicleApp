b4c7929aee60a0aa39806d6b40a68673c224f739
Tomás Aguiar <aguiarhcode@gmail.com>
2022-06-04 19:48:14 +0100

    Modal to delete servides

--- src/components/activeServiceComponent/activeServiceComponent.js
+++ src/components/activeServiceComponent/activeServiceComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {DotsThree} from 'phosphor-react-native';
+import {View, Text, TouchableOpacity, Modal} from 'react-native';
+import {DotsThree, Trash} from 'phosphor-react-native';
 
 const ActiveServiceComponent = ({item}) => {
   const [days, setDays] = React.useState(0);
   const [percentageToChange, setPercentageToChange] = React.useState();
+  const [modalVisible, setModalVisible] = React.useState(false);
 
   const handleDate = () => {
     let seconds = item.ends.seconds - item.starts.seconds;
@@ -40,7 +41,7 @@ const ActiveServiceComponent = ({item}) => {
         </View>
         <View>
           <TouchableOpacity
-            onPress={() => {}}
+            onPress={() => setModalVisible(!modalVisible)}
             style={{
               alignItems: 'center',
               justifyContent: 'center',
@@ -58,6 +59,36 @@ const ActiveServiceComponent = ({item}) => {
         }}>
         <Text>{days} dias para efetuar a troca.</Text>
       </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={{flex: 1, backgroundColor: 'rgba(0,0,0,0.5)'}}>
+          <View
+            style={{
+              backgroundColor: '#2c2c2c',
+              borderRadius: 5,
+              padding: 20,
+              marginTop: '170%',
+              marginHorizontal: 38,
+            }}>
+            <TouchableOpacity
+              style={{
+                justifyContent: 'center',
+              }}>
+              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <Trash size={25} color={'#aeaeae'} />
+                <Text style={{fontSize: 15, marginLeft: 5}}>
+                  Remover serviço
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
